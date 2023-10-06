94405dc6dbe1304204d89365fb2ce8f1d1244a78
ALA YOUNES <ala.younes@eviden.com>
2023-10-06 11:18:39 +0200

    feat: image optimisation from nextJs Image docs

--- src/app/components/ImageContainer.tsx
+++ src/app/components/ImageContainer.tsx
@@ -7,8 +7,15 @@ type Props = {
 
 const ImageContainer = ({ photo }: Props) => {
   return (
-    <div className="h-64 bg-gray-200 rounded-xl">
-      <Image src={photo.src.large} alt={photo.alt} width={250} height={250} />
+    <div className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group">
+      <Image
+        src={photo.src.large}
+        alt={photo.alt}
+        fill={true}
+        className="object-cover group-hover:opacity-75"
+        priority={true}
+        sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+      />
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Gallery from "./components/Gallery";
 export default function Home() {
   return (
     <div className="">
-      <Gallery />;
+      <Gallery />
     </div>
   );
 }
